46cbe2f3a14d8601bcb51419abe2c45447bee9bf
Luis Rosales <luismrosales2@gmail.com>
2021-08-15 22:42:06 -0400

    conditions to make decisions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,54 @@
-function fight() {
-    window.alert("the fight has begun!");
-    
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+var fight = function() {
+    //alert players that they are starting the round
+    window.alert("welcome to robot gladiators!");
+    enemyHealth = enemyHealth -playerAttack;
+    console.log(playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining");
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+
+    }
+
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left ");
+    }
+
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + " . " + playerName + "  now has " + playerHealth + " health remaining");
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+
+    else {
+        window.alert (playerName + " still has " + playerHealth + " health left");
+    }
 }
-//fight();
 
-var playerName = window.prompt("what is your robots name?");
-console.log(playerName);
+//subtract the value of player attack from the 
 
-console.log("this logs a string, good for leaving yourself a message ");
-//this will do math and log 20
-console.log(10+10);
-//what is this?
-console.log("our robot name is" + playerName);
+fight();
+
+
+/*
+1 Alert players that they're starting the round (this is already done).
+
+2 Subtract the value of playerAttack from the value of enemyHealth, and use that result to update the value in the enemyHealth variable.
+
+3 Log a resulting message to the console to confirm that it worked.
+
+4 Subtract the value of enemyAttack from the value of playerHealth, and use that result to update the value in the playerHealth variable.
+
+5 Log a resulting message to the console to confirm that it worked. */
+
