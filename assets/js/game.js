c10b34bc54135bb920c8a34fd0cc7906073ce3f5
Luis Rosales <luismrosales2@gmail.com>
2021-08-16 21:29:09 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,12 +4,24 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 
-var enemyName = "Roborto";
+var enemyNames = ["roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 
-var fight = function() {
+
+//game states
+// "WIN" - player robot has defeated all enemy-robots
+// * fight all enemy-robots
+// * defeat each enemy-robot
+
+
+
+// "LOSE" -player robot's health is zero or less
+
+var fight = function(enemyName) {
+  //fight function statements
+
     //alert players that they are starting the round
     window.alert("welcome to robot gladiators!");
     var promptFight = window.prompt("would you like to fight or skip this battle? Enter 'fight' or 'skip' to choose.");
@@ -84,20 +96,14 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 }
 
 
+for(var i = 0; i < enemyNames.length; i++){
+  fight(enemyNames[i]);
+}
+
+
 
 //subtract the value of player attack from the 
 
-fight();
+//fight();
 
-
-/*
-1 Alert players that they're starting the round (this is already done).
-
-2 Subtract the value of playerAttack from the value of enemyHealth, and use that result to update the value in the enemyHealth variable.
-
-3 Log a resulting message to the console to confirm that it worked.
-
-4 Subtract the value of enemyAttack from the value of playerHealth, and use that result to update the value in the playerHealth variable.
-
-5 Log a resulting message to the console to confirm that it worked. */
 
